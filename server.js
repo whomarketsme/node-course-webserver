b3d3ff9a1b9eaee5054394f8e7d7a446a6871d3e
SE <sk@SEs-MacBook-Pro.local>
2018-11-19 23:44:18 +0100

    commented out maintenance mode

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+//
+// });
 
 app.use(express.static(__dirname + '/public')) //static requires an absolute path
 
